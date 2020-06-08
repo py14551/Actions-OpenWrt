a41be662247bed1d6faf03b648ce989893607e19
hippoepic <31174624+py14551@users.noreply.github.com>
2020-06-08 17:27:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
+sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+sed -i '30a tools-y += ucl upx' tools/Makefile
+sed -i '44a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
